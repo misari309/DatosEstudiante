2f44d9f65c6978b5a145cde02e589838f29b5bce
misari309 <misari309@gmail.com>
2021-09-11 15:07:54 -0500

    Se agregan todos los archivos

---
+++ src/datosestudiante/DatosEstudiante.java
@@ -0,0 +1,10 @@
+package datosestudiante;
+
+public class DatosEstudiante {
+
+    public static void main(String[] args) {
+        FramePrincipal obj1 = new FramePrincipal();
+        obj1.setVisible(true);
+    }
+    
+}
---
+++ src/datosestudiante/Estudiante.java
@@ -0,0 +1,15 @@
+package datosestudiante;
+
+
+public class Estudiante {
+    static int codigo;
+    static String nombre;
+    static int telefono;
+    
+    public Estudiante(int codigo, String nombre, int telefono){
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this. telefono = telefono;
+    }
+    
+}
